5350a222f7107ac9ef99c5ef65951c5ef135f1f5
zalfrie januardi <zalfrie@protonmail.com>
2022-01-21 13:31:30 +0700

    add graphQL Queries

---
+++ app/GraphQL/Queries/Outlet/OutletQuery.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\GraphQL\Queries\Outlet;
+
+use App\Models\Outlet;
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Facades\GraphQL;
+use Rebing\GraphQL\Support\Query;
+
+class OutletQuery extends Query
+{
+    protected $attributes = [
+        'name' => 'outlet',
+    ];
+
+    public function type(): Type
+    {
+        return GraphQL::type('Outlet');
+    }
+
+    public function args(): array
+    {
+        return [
+            'id' => [
+                'name' => 'id',
+                'type' => Type::int(),
+                'rules' => ['required']
+            ]
+        ];
+    }
+
+    public function resolve($root, $args)
+    {
+        return Outlet::findOrFail($args['id']);
+    }
+}
